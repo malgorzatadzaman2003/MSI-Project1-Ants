975885ea87845e31385a36d72604aed4909595d2
Małgorzata Dżaman <malgorzatadzaman@MacBook-Pro-Magorzata.local>
2026-04-09 13:00:52 +0200

    add cyrp_instance route_length

--- msi-project1-ants/src/cvrp_instance.py
+++ msi-project1-ants/src/cvrp_instance.py
@@ -22,3 +22,21 @@ class CVRPInstance:
         x1, y1 = self.node_coords[a]
         x2, y2 = self.node_coords[b]
         return math.dist((x1, y1), (x2, y2))
+    
+    def route_length(self, route: List[int]) -> float:
+        """
+        Liczy długość pojedynczej trasy.
+        Zakładamy, że route zawiera klientów, np. [1, 5, 3].
+        Trasa liczona jest jako depot -> ... -> depot.
+        """
+        if not route:
+            return 0.0
+
+        total = 0.0
+        prev = self.depot_id
+        for customer in route:
+            total += self.distance(prev, customer)
+            prev = customer
+        total += self.distance(prev, self.depot_id)
+        return total
+    
